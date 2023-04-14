5b5790035104a1fa2210182aa501803e58efaacf
Diego R Dantas <112526586+DiegoDantas02@users.noreply.github.com>
2023-04-14 11:55:57 -0300

    Add project files.

---
+++ calcule-a-idade/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite a idade em anos: ");
+        int anos = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite a idade em meses: ");
+        int meses = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite a idade em dias: ");
+        int dias = int.Parse(Console.ReadLine());
+
+        int idadeEmDias = anos * 365 + meses * 30 + dias;
+
+        Console.WriteLine("A idade em dias é: " + idadeEmDias);
+
+        Console.ReadLine();
+    }
+}
